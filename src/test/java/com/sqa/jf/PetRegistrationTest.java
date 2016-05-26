20ba7a804274ab783c2f9b6321c9cf6367cda3d2
Jean-francois Nepton <sqasolution@hotmail.com>
2016-05-25 20:14:41 -0700

    Create Test for PetRegistration App

---
+++ src/test/java/com/sqa/jf/PetRegistrationTest.java
@@ -0,0 +1,37 @@
+/**
+ *   File Name: PetRegistrationTest.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: May 25, 2016
+ *
+ */
+
+package com.sqa.jf;
+
+import org.junit.*;
+
+/**
+ * PetRegistrationTest //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class PetRegistrationTest {
+
+	/**
+	 * Test method for {@link com.sqa.jf.PetRegistration#registerPet()}.
+	 */
+	@Test
+	public void testRegisterPet() {
+		PetRegistration.registerPet();
+	}
+
+}
